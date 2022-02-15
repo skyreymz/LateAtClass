ee006e22eb98349a99f2db8bef3541a37748b168
skyreymz <remycauvin@live.fr>
2022-02-15 22:03:48 +0100

    adding plants

--- Assets/GistLevelDesignerFree/Editor/Utility/Utility.cs
+++ Assets/GistLevelDesignerFree/Editor/Utility/Utility.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityEditor.Experimental.SceneManagement;
+
 using System.Reflection;
 using System.Collections.Generic;
 
@@ -17,7 +17,7 @@ namespace GistLevelDesignerFree {
             gameObjectIdentity.fileID = 0;
             gameObjectIdentity.rootGUID = null;
             
-            PrefabStage prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            UnityEditor.SceneManagement.PrefabStage prefabStage = UnityEditor.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
             if (prefabStage != null || PrefabUtility.IsPartOfAnyPrefab(gameObject)) {
                 prefabIdentity = true;
                 gameObjectIdentity.identity = Identity.ASSET;
